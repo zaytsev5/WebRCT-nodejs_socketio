1026a1823ae990b15a20de67a56182b8c893d5e1
zaytsev5 <55730701+zaytsev5@users.noreply.github.com>
2020-04-28 21:40:12 +0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", "./views");
-
+//chat
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
 server.listen(3000);
